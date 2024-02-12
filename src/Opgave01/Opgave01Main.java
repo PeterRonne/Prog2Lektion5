111df673d725ed90186cdef796c2bb75361a79a9
Peter Roest Rønne <77733864+PeterRonne@users.noreply.github.com>
2024-02-12 08:46:18 +0100

    opgave 01

--- src/Opgave01/Opgave01Main.java
+++ src/Opgave01/Opgave01Main.java
@@ -1,7 +1,15 @@
 package Opgave01;
 
 public class Opgave01Main {
+
+    public static void main(String[] args) {
+        System.out.println(factorial(5));
+    }
+
     public static int factorial(int n) {
-        return -1;
+        if (n == 1)
+            return n;
+
+        return n * factorial(n - 1);
     }
 }
