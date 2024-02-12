17d1dc88682f6e1a4f3f468373fffcdbbc1d66f1
Peter Roest Rønne <77733864+PeterRonne@users.noreply.github.com>
2024-02-12 09:19:25 +0100

    opgave 03

--- src/Opgave03/Opgave03Main.java
+++ src/Opgave03/Opgave03Main.java
@@ -1,7 +1,15 @@
 package Opgave03;
 
 public class Opgave03Main {
+
+    public static void main(String[] args) {
+        System.out.println(power(2,4));
+
+    }
     public static int power(int n, int p) {
-        return -1;
+        if (p == 0)
+            return 1;
+
+        return power(n, (p-1)) * n;
     }
 }
