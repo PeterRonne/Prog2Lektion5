fc674bf4315efe1362704be0882a87bf7d164cb6
Peter Roest Rønne <77733864+PeterRonne@users.noreply.github.com>
2024-02-12 10:36:22 +0100

    opgave 02 optimeret

--- src/Opgave02/Opgave02Main.java
+++ src/Opgave02/Opgave02Main.java
@@ -1,14 +1,27 @@
 package Opgave02;
 
-public class Opgave02Main {
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
+public class Opgave02Main {
+private static long[] fibonacciStorage;
     public static void main(String[] args) {
-        System.out.println(fibonacci(6));
+        int n = 92;
+        fibonacciStorage = new long[n + 1];
+
+        System.out.println(fibonacci(10));
+        System.out.println(Arrays.toString(fibonacciStorage));
     }
     public static int fibonacci(int n) {
         if (n <= 1)
             return n;
 
-        return fibonacci(n - 1) + fibonacci(n - 2);
+        if (fibonacciStorage[n] != 0) {
+            return (int) fibonacciStorage[n];
+        }
+
+        int nthFibonacciNumber = fibonacci(n - 1) + fibonacci(n - 2);
+        fibonacciStorage[n] = nthFibonacciNumber;
+        return nthFibonacciNumber;
     }
 }
--- src/Opgave04/Opgave04Main.java
+++ src/Opgave04/Opgave04Main.java
@@ -2,6 +2,7 @@ package Opgave04;
 
 public class Opgave04Main {
     public static String reverse(String s) {
+
         return "RemoveMe";
     }
 }
