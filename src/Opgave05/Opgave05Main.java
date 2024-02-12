6aa078821079b4de164879b23d97c4dffb39b73a
Peter Roest Rønne <77733864+PeterRonne@users.noreply.github.com>
2024-02-12 13:01:55 +0100

    opgave 05 -u

--- src/Opgave05/Opgave05Main.java
+++ src/Opgave05/Opgave05Main.java
@@ -2,9 +2,8 @@ package Opgave05;
 
 public class Opgave05Main {
     public static void main(String[] args) {
-        System.out.println(sfd(28,12));
+        System.out.println(sfd(36,48));
     }
-
     public static int sfd(int a, int b) {
         if (b == 0) {
             return a;
