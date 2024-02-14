1035ab546b20d904204cbd1d7f39a2e409b8c956
Peter Roest Rønne <77733864+PeterRonne@users.noreply.github.com>
2024-02-14 23:02:25 +0100

    opgave 3 ekstra opagave løst

--- src/Opgave03/Opgave03Main.java
+++ src/Opgave03/Opgave03Main.java
@@ -3,13 +3,37 @@ package Opgave03;
 public class Opgave03Main {
 
     public static void main(String[] args) {
-        System.out.println(power(2,4));
+        System.out.println(power(2, 5));
 
     }
+
+//    public static int power(int n, int p) {
+//        if (p == 0)
+//            return 1;
+//
+//        return power(n, (p - 1)) * n;
+//    }
+
     public static int power(int n, int p) {
-        if (p == 0)
+        if (p == 0) {
             return 1;
-
-        return power(n, (p-1)) * n;
+        } else if (p % 2 == 0) {
+            int temp = power(n, p / 2);
+            return temp * temp;
+        } else {
+            return power(n, p - 1) * n;
+        }
     }
+
+//    public static int power(int n, int p) {
+//        if (p == 0)
+//            return 1;
+//
+//        int temp = power(n, p / 2);
+//        if (p % 2 == 0)
+//            return temp * temp;
+//        else
+//            return temp * temp * n;
+//    }
+
 }
